31fc47f1b5f087d5889c40cfdfc4b623feffcd18
Musab Kılıç <musav70@gmail.com>
2018-02-02 21:35:58 +0300

    Update matrix.test.js

--- matrix.test.js
+++ matrix.test.js
@@ -33,3 +33,27 @@ test('product test',()=>{
     ]
   });
 });
+
+test('adding matrix to matrix', () => {
+  let m1 = new Matrix(3, 3);
+  m1.data[0] = [1, 2, 3];
+  m1.data[1] = [4, 5, 6];
+  m1.data[2] = [7, 8, 9];
+  
+  let m2 = new Matrix(3,3);
+  m2.data[0] = [7, 5, 1];
+  m2.data[1] = [0, 5, 9];
+  m2.data[2] = [4, 2, 9];
+  
+  m1.add(m2);
+  
+  expect(m1).toEqual({
+    rows: 3,
+    cols: 3,
+    data: [
+      [8, 7, 4],
+      [4, 10, 15],
+      [11, 10, 18]
+    ]
+  });
+});
