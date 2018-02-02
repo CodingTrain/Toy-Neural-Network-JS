929474b7e08e8d5a3954e8620ecc1388adf0c288
Daniel Shiffman <daniel@shiffman.net>
2018-02-02 13:14:10 -0500

    adding unit tests with circleci

---
+++ matrix.test.js
@@ -0,0 +1,18 @@
+const Matrix = require('./Matrix');
+
+test('adding scalar to matrix', () => {
+  let m = new Matrix(3, 3);
+  m.data[0] = [1, 2, 3];
+  m.data[1] = [4, 5, 6];
+  m.data[2] = [7, 8, 9];
+  m.add(1);
+  expect(m).toEqual({
+    rows: 3,
+    cols: 3,
+    data: [
+      [2, 3, 4],
+      [5, 6, 7],
+      [8, 9, 10]
+    ]
+  });
+});
