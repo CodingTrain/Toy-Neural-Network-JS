d21ec87a7d330f4c73b5971ff32a1bdc1e9e7777
Musab Kılıç <musav70@gmail.com>
2018-02-02 21:30:49 +0300

    Update matrix.test.js

--- matrix.test.js
+++ matrix.test.js
@@ -16,3 +16,25 @@ test('adding scalar to matrix', () => {
     ]
   });
 });
+
+test('adding matrix to matrix', () => {
+  let m1 = new Matrix(3, 3);
+  m1.data[0] = [1, 2, 3];
+  m1.data[1] = [4, 5, 6];
+  m1.data[2] = [7, 8, 9];
+  
+  let m2 = new Matrix(3,3);
+  m1.data[0] = [7, 5, 1];
+  m1.data[1] = [0, 5, 9];
+  m1.data[2] = [4, 2, 9];
+  
+  expect(m).toEqual({
+    rows: 3,
+    cols: 3,
+    data: [
+      [8, 7, 4],
+      [4, 10, 15],
+      [11, 10, 18]
+    ]
+  });
+});
