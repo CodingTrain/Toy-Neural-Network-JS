aeba18a745b94903662c545364f29d282324662e
Maksuel Boni <maksuel@gmail.com>
2018-10-07 01:49:00 +0200

    clear

--- examples/doodle_classification/visualization.js
+++ examples/doodle_classification/visualization.js
@@ -1,19 +1,36 @@
 class Visualization {
-    constructor(nn) {
+
+    static _check(nn) {
         if(typeof p5 !== 'function') {
             throw new Error('Need to include p5js');
         } else if(!nn instanceof NeuralNetwork) {
             throw new Error('Need a instance of NeuralNetwork');
         }
+    }
 
+    static _getLayers(nn) {
         let layers = [];
 
         for(let layer of nn.layers) {
             layers.push(layer.nodes);
         }
 
-        console.log('Visualization',layers);
+        return layers;
+    }
 
-        // this.graphics = createGraphics();
+    static graphics(nn) {
+        this._check(nn);
+
+        let layers = this._getLayers(nn);
+
+        let w = floor(layers.length * 20);
+        let h = floor(layers[0] * 20);
+
+        let graphics = createGraphics(w,h);
+
+        console.log(graphics.width, graphics.height);
+
+        return graphics;
+
     }
 }
